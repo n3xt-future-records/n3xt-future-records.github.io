8a0b024281472c2f43afc7b44520a18d01357718
n3xt-future-records <109465145+n3xt-future-records@users.noreply.github.com>
2022-07-17 21:05:40 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(function () {
+
+  //ページ内スクロール
+  var $nav = $(".header");
+  var navHeight = $nav.outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate(
+      {
+        scrollTop: position,
+      },
+      300,
+      "swing"
+    );
+    return false;
+  });
+
+  //スクロールに応じてヘッダーの背景色が変化
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $('.header-bg').addClass('active');
+      $('.mv-video').addClass('active');
+    } else {
+      $('.header-bg').removeClass('active');
+      $('.mv-video').removeClass('active');
+    }
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      300
+    );
+    return false;
+  });
+});
